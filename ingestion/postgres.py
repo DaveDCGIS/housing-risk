3640265715faf63bea64d7dcc0357327122aa594
NealHumphrey <humphrey.neal@gmail.com>
2016-10-22 18:03:08 -0400

    Current status:
    -Snapshots selected in the snapshots_manifest.csv are loading properly into my local copy of the postgres database
    -some of the snapshots marked as 'skip' are just to reduce sample load size;
    -some of the snapshots are having 'nan' errors in the date field. There is commented out code on how to handle this with a custom date parser but we need to verify that the date format works for all relevant columns.
    -some of the snapshots (2003 was one) are having errors in the source data - weird characters, and too-long rows of data. Haven't yet investigated the cause.

--- ingestion/postgres.py
+++ ingestion/postgres.py
@@ -28,7 +28,7 @@ logging.getLogger().addHandler(logging.StreamHandler())
 #############################
 constants = {
     'secrets_filename': 'secrets.json',
-    'manifest_filename': 'temp_snapshots_test.csv',
+    'manifest_filename': 'snapshots_manifest.csv',
     'date_headers_filename': 'postgres_date_headers.json',
 }
 
@@ -55,37 +55,29 @@ def csv_to_sql(index_path):
     engine = create_engine(connect_str)
 
     for index, row in paths_df.iterrows():
-        full_path = row['path'] + row['filename']
-        tablename = row['table_name']
+        if row['skip'] == "skip":
+            logging.info("skipping table " + str(row['snapshot_id']))
+        else:
+            full_path = row['path'] + row['filename']
+            tablename = row['table_name']
 
-        logging.info("loading table " + str(index + 1) + " (" + tablename + ")")
+            logging.info("loading table " + str(index + 1) + " (" + tablename + ")")
 
-        #Since different files have different date columns, we need to compare to the master list.
-        headers = list(get_column_names(full_path))
-        with open(constants['date_headers_filename']) as fh:
-            date_headers = json.load(fh)
-            parseable_headers = date_headers['date_headers']
-        to_parse = list(set(parseable_headers) & set(headers))
-        logging.info("  identified date columns: " + str(to_parse))
+            #Since different files have different date columns, we need to compare to the master list.
+            headers = list(get_column_names(full_path))
+            with open(constants['date_headers_filename']) as fh:
+                date_headers = json.load(fh)
+                parseable_headers = date_headers['date_headers']
+            to_parse = list(set(parseable_headers) & set(headers))
+            logging.info("  identified date columns: " + str(to_parse))
 
-        csv_df = pd.read_csv(full_path, encoding="latin1", parse_dates=to_parse) #If we have null dates that are non-blank (coded nulls), need to use this: date_parser=parser #parser = lambda x: pd.to_datetime(x, format='%m/%d/%Y', errors='coerce')
-        logging.info("  in memory...")
+            csv_df = pd.read_csv(full_path, encoding="latin1", parse_dates=to_parse) #If we have null dates that are non-blank (coded nulls), need to use this: date_parser=parser #parser = lambda x: pd.to_datetime(x, format='%m/%d/%Y', errors='coerce')
+            logging.info("  in memory...")
 
-        #Add a column so that we can put all the snapshots in the same table
-        csv_df['snapshot_id'] = row['snapshot_id']
-        csv_df.to_sql(tablename, engine, if_exists='append')
-        logging.info("  table loaded")
-
-def access_to_sql():
-    '''
-    Currently not working - can't get driver...
-    '''
-    import pyodbc
-    DBfile = "../data/section8/contracts_database/main-website/MF_Assistance_&_Sec8_Contracts_2016-08-02.mdb"
-    conn = pyodbc.connect('DRIVER={Microsoft Access Driver (*.mdb)};DBQ='+DBfile)
-    #or try this below if using with Access 2007, 2010 .accdb file
-    #conn = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ='+DBfile)
-    cursor = conn.cursor()
+            #Add a column so that we can put all the snapshots in the same table
+            csv_df['snapshot_id'] = row['snapshot_id']
+            csv_df.to_sql(tablename, engine, if_exists='append')
+            logging.info("  table loaded")
 
 if __name__ == '__main__':
     #sample_add_to_database()
