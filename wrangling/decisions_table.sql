236cdef0a65c1a1f2432ccafcfa0fd1503b3d6ee
NealHumphrey <humphrey.neal@gmail.com>
2016-11-05 12:51:48 -0400

    Added expiration test using manifest
    Did additional test and code cleanup

--- wrangling/decisions_table.sql
+++ wrangling/decisions_table.sql
@@ -1,26 +1,55 @@
-﻿--select distinct snapshot_id from contracts;
+﻿--select distinct tracs_status_name from contracts;
 
 SELECT *
 FROM(
 
 	SELECT 
-		CASE WHEN EXTRACT(epoch FROM time_diff)/3600 > 0 THEN 'in'
-		     WHEN EXTRACT(epoch FROM time_diff)/3600 = 0 THEN 'no change'
-		  else null
-		  END AS expiration_test
-		, CASE WHEN tracs_status_name = 'Expired' 
-			AND previous_status = 'Active'
-			THEN 'out' 
+		--Has the expiration been extended?
+		  CASE WHEN EXTRACT(epoch FROM time_diff)/3600 > 0 
+		       THEN 'in'
+		     WHEN EXTRACT(epoch FROM time_diff)/3600 = 0 
+		       THEN 'no change'
+		     WHEN time_diff IS NULL
+		       THEN 'first'
+		     ELSE 'flag'
+		  END AS expiration_extended_test
+
+		--Did it change from Active to one of the cancelled statuses?
+		, CASE 	WHEN tracs_status_name IN ('Terminated', 'Expired', 'Cancelled', 'Suspended')
+			  AND previous_status = 'Active'
+			  THEN 'out'
+			WHEN tracs_status_name = previous_status
+			  THEN 'no change'
+			WHEN previous_status IS NULL
+			  THEN 'first'
+			ELSE 'not handled'
 		  END AS status_test
+
+		--Has the contract expired as of the day the snapshot was downloaded?
+		, CASE 	WHEN expiration_passed_check = 'Expired'
+			  AND previous_expiration_passed_check = 'Ok'
+			  THEN 'out'
+			WHEN expiration_passed_check = previous_expiration_passed_check
+			  THEN 'no change'
+			WHEN previous_expiration_passed_check IS NULL
+			  THEN 'first'
+			ELSE 'flag'
+		  END AS expiration_passed_test
 		, *
 	FROM (
 		SELECT 
-		  TRIM(tracs_status_name) as tracs_status_name
+		
+		  
+		  TRIM(LAG(expiration_passed_check,1) 
+			    OVER (partition by contract_number order by snapshot_id)) as previous_expiration_passed_check
+		, expiration_passed_check
+		, TRIM(tracs_status_name) as tracs_status_name
 		, TRIM(LAG(tracs_status_name,1) 
 			    OVER (partition by contract_number order by snapshot_id)) as previous_status
 		, (tracs_overall_expiration_date - lag(tracs_overall_expiration_date, 1) OVER (partition by contract_number order by snapshot_id)) as time_diff
 		, contract_number
 		, snapshot_id
+		, snapshot_date
 		, contract_term_months_qty
 		, tracs_effective_date
 		, tracs_overall_expiration_date
@@ -30,7 +59,7 @@ FROM(
 		FROM (
 			SELECT contract_number
 			, property_id, property_name_text
-				, snapshot_id
+				, contracts.snapshot_id AS snapshot_id
 				, tracs_effective_date
 				, tracs_overall_expiration_date
 				, tracs_overall_exp_fiscal_year
@@ -38,8 +67,17 @@ FROM(
 				, tracs_current_expiration_date
 				, tracs_status_name
 				, contract_term_months_qty
+				, manifest.date AS snapshot_date
+
+				, CASE 	WHEN manifest.date > tracs_overall_expiration_date 
+					THEN 'Expired'
+					ELSE 'Ok'
+				  END AS expiration_passed_check
+				  
 			  FROM public.contracts
-			  WHERE contract_number = '012063NISUP' --'OH16Q921001' 
+			  LEFT JOIN public.manifest
+			  ON public.contracts.snapshot_id = public.manifest.snapshot_id
+			  WHERE contract_number = '012063NISUP' --'OH16Q921001' --
 		  ) as subtable
 	) AS decision_table
 ) AS final_table
