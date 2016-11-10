5b9ae8079ec0a4f7d41dc97cb60a598800fbacaa
NealHumphrey <humphrey.neal@gmail.com>
2016-11-10 15:38:49 -0500

    --Rearranged SQL to use WITH statements for the decisions table. Allows easier query re-use as we dig into data wrangling.
    --Set up several options for filtering results
    --Created visualization for the 100 contracts with the earliest observed expiration dates across all snapshots.
    
    snapshots_manifest updated to reflect Neal's local copy loading status.

--- wrangling/decisions_table.sql
+++ wrangling/decisions_table.sql
@@ -1,22 +1,12 @@
 ﻿
---These are just the fields we want to load as our decision table
-SELECT 	
-	
-	  snapshot_id
-	, contract_number
-	--This is a temporary approach to choosing among our various tests
-	, CASE WHEN expiration_extended_test = 'in' THEN 'in'
-		WHEN status_test = 'out' THEN 'out'
-		WHEN expiration_extended_test = 'no change' then 'no change'
-		ELSE 'other'
-	  END AS decision
-	, tracs_overall_expiration_date
-	, time_diff
-	, contract_term_months_qty
-	, tracs_status_name
-	, previous_status
-	  
-FROM(
+/*
+-------------------------
+The 'decisions_tests' table compares each snapshot to the previous one,
+and returns the results of the various tests. It is then necessary to 
+decide which tests we want to use to create the actual 'decisions' table.
+-------------------------
+*/
+WITH decisions_tests AS (
 	--This is our full report of decision tests
 	SELECT 
 		--Has the expiration been extended?
@@ -92,22 +82,127 @@ FROM(
 			  FROM public.contracts
 			  LEFT JOIN public.manifest
 			  ON public.contracts.snapshot_id = public.manifest.snapshot_id
-			  --WHERE contract_number = '012063NISUP' --'OH16Q921001' --
 		  ) as subtable
 	) AS decision_table
-) AS final_table
---Optionally, filter the results
+) 
+
+	/*
+	--------------------------
+	contract_number_queries
+	These are virtual tables we can plug into the 'where in(virtual_table)' statement to filter down to only some contracts. 
+	--------------------------*/
+	, contracts_earliest100 AS (
+	SELECT contract_number
+		FROM contracts
+		GROUP BY contract_number
+		ORDER BY MIN(tracs_overall_expiration_date) ASC
+		LIMIT 100
+	)
+
+	, contracts_latest100 AS (
+	SELECT contract_number		--, MIN(tracs_overall_expiration_date) 
+		FROM contracts
+		GROUP BY contract_number
+		ORDER BY MIN(tracs_overall_expiration_date) DESC
+		LIMIT 100
+	)
+
+	, contracts_random100 AS (
+	SELECT contract_number
+	FROM	(
+		SELECT contract_number, MIN(tracs_overall_expiration_date) AS earliest_expiration
+			FROM contracts
+			GROUP BY contract_number
+			LIMIT 100
+		) as random_unsorted
+	ORDER BY earliest_expiration
+	)
+
+	--This one can't be used by itself b/c of the select date
+	, all_contracts_sorted AS (
+	SELECT    contract_number 
+		, MIN(tracs_overall_expiration_date) AS earliest_expiration
+	FROM contracts
+	GROUP BY contract_number
+	ORDER BY MIN(tracs_overall_expiration_date) ASC
+)
+
+	--This is a way to paginate our results so that we can split our contracts into multiple files
+	, contracts_500_1 AS (
+	SELECT contract_number
+	FROM (
+		SELECT  ROW_NUMBER() OVER ( ORDER BY earliest_expiration ) AS rownum
+			, contract_number
+			, earliest_expiration
+		FROM all_contracts_sorted
+		) AS row_number_added
+	WHERE   rownum >= 1
+	    AND rownum <= 500 
+	ORDER BY RowNum
+	)
+
+	, contracts_500_2 AS (
+	SELECT contract_number
+	FROM (
+		SELECT  ROW_NUMBER() OVER ( ORDER BY earliest_expiration ) AS rownum
+			, contract_number
+			, earliest_expiration
+		FROM all_contracts_sorted
+		) AS row_number_added
+	WHERE   rownum >= 501
+	    AND rownum <= 1000 
+	ORDER BY RowNum
+	)
+
+	/*
+	--------------------------
+	end of contract_number_queries
+	--------------------------*/
+
+--Query to view the decision_tests table
+/*
+SELECT * 
+FROM decisions_tests
+WHERE contract_number IN ( select * from contracts_random100)
+*/
+
+
+------------------
+--This is the query that returns decisions
+------------------
+--Choose which tests we want to use to create decisions
+SELECT 	  snapshot_id
+	, contract_number
+	--This is a temporary approach to choosing among our various tests
+	, CASE WHEN expiration_extended_test = 'in' THEN 'in'
+		WHEN status_test = 'out' THEN 'out'
+		WHEN expiration_extended_test = 'no change' then 'no change'
+		ELSE 'other'
+	  END AS decision
+	, tracs_overall_expiration_date
+	, time_diff
+	, contract_term_months_qty
+	, tracs_status_name
+	, previous_status
+
+FROM decisions_tests
+
+--Optionally, filter the decisions results
 --WHERE expiration_test = 'in' 
 --	OR status_test = 'out'
 
---Optionally, get just a subset of contract_numbers
+--Optionally, get just a subset of contract_numbers. This sorts by earliest expiration for each contract. 
 WHERE contract_number 
-IN (
-	SELECT DISTINCT contract_number
-	FROM contracts
-	LIMIT 200
+IN ( 	select * from
+	contracts_earliest100
+
+	--Options for filtering, from contract_number_queries section above:
+	--contracts_earliest100
+	--contracts_latest100
+	--contracts_random100
+	--contracts_500_1
+	--contracts_500_2
+	--'012063NISUP' 
+	--'OH16Q921001'
 	)
---LIMIT 1000
-
-
 ;
